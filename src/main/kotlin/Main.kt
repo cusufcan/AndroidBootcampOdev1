67e5f32e0c5ede42552e7486504bf5579d353836
cusufcan <cusufcan@gmail.com>
2025-04-25 21:50:42 +0300

    Finish

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,141 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val sehir = "Sakarya"
+    println("Şehir: $sehir")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    val ulke = "Türkiye"
+    println("Ülke: $ulke")
+
+    val telefon = "05528884560"
+    println("Telefon: $telefon")
+
+    val postaKodu = "54000"
+    println("Posta Kodu: $postaKodu")
+
+    val email = "cusufcan@gmail.com"
+    println("Email: $email")
+
+    val meslek = "Android Developer"
+    println("Meslek: $meslek")
+
+    val stokMiktari = 400
+    println("Stok Miktarı: $stokMiktari")
+
+    val musteriAdi = "Yusuf Can Mercan"
+    println("Müşteri Adı: $musteriAdi")
+
+    val bakiye = 1000.00
+    println("Bakiye: $bakiye")
+
+    val dogumGunu = "01.08.2003"
+    println("Doğum Günü: $dogumGunu")
+
+    val maas = 1000.00
+    println("Maaş: $maas")
+
+    val medeniDurum = "Bekar"
+    println("Medeni Durum: $medeniDurum")
+
+    val urunYorum = "Çok güzel bir ürün."
+    println("Ürün Yorum: $urunYorum")
+
+    val odemeTarihi = "20.08.2023"
+    println("Ödeme Tarihi: $odemeTarihi")
+
+    val odeme = 1000.00
+    println("Ödeme: $odeme")
+
+    val siparisAdeti = 2
+    println("Sipariş Adeti: $siparisAdeti")
+
+    val arabaModeli = "BMW M3"
+    println("Araba Modeli: $arabaModeli")
+
+    val kitapAdi = "Satranç"
+    println("Kitap Adı: $kitapAdi")
+
+    val yayinlamaTarihi = "20.08.2023"
+    println("Yayınlanma Tarihi: $yayinlamaTarihi")
+
+    val indirimMiktari = 10
+    println("İndirim Miktarı: $indirimMiktari")
+
+    val odaSayisi = 2
+    println("Oda Sayısı: $odaSayisi")
+
+    val enlem = 41.012788
+    println("Enlem: $enlem")
+
+    val boylam = 29.294579
+    println("Boylam: $boylam")
+
+    val urunAdi = "Araba"
+    println("Ürün Adı: $urunAdi")
+
+    val yemekFiyati = 10.5
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    val marka = "BMW"
+    println("Marka: $marka")
+
+    val muzikAdi = "Kalbine"
+    println("Müzik Adı: $muzikAdi")
+
+    val videoSuresi = 64
+    println("Video Süresi: $videoSuresi")
+
+    val urunPuani = 5.9
+    println("Ürün Puanı: $urunPuani")
+
+    val resimAdi = "Yıldızlı Gece"
+    println("Resim Adı: $resimAdi")
+
+    val dosyaFormati = "jpg"
+    println("Dosya Formatı: $dosyaFormati")
+
+    val renk = "siyah"
+    println("Renk: $renk")
+
+    val renkKodu = 0xFF000000
+    println("Renk Kodu: $renkKodu")
+
+    val telefonModeli = "iPhone 14"
+    println("Telefon Modeli: $telefonModeli")
+
+    val ekranBoyutu = 6.5
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    val agirlik = 120
+    println("Ağırlık: $agirlik")
+
+    val ulusalGun = "23 Nisan"
+    println("Ulusal Gün: $ulusalGun")
+
+    val tatilGunu = "24 Nisan"
+    println("Tatil Günü: $tatilGunu")
+
+    val rezervasyonTarihi = "23 Nisan 2023"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+
+    val sokakAdi = "Kalecik"
+    println("Sokak Adı: $sokakAdi")
+
+    val otobusHatti = "1"
+    println("Otobüs Hattı: $otobusHatti")
+
+    val kalanDakika = 10
+    println("Kalan Dakika: $kalanDakika")
+
+    val takipKodu = "34343"
+    println("Takip Kodu: $takipKodu")
+
+    val kuponSuresi = 40
+    println("Kupon Süresi: $kuponSuresi")
+
+    val kuponKodu = "123456789"
+    println("Kupon Kodu: $kuponKodu")
+
+    val faturaAdresi = "Manisa"
+    println("Fatura Adresi: $faturaAdresi")
 }
